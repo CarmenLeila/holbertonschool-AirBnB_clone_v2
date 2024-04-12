f2af88b38454c3c8880a72db53bc58a149002ebc
CarmenLeila <146730823+CarmenLeila@users.noreply.github.com>
2024-04-12 12:56:23 +0200

    4.script that starts a Flask web application

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+""" Starts a Flask web application """
+
+from flask import Flask
+from markupsafe import escape
+
+
+app = Flask('__name__')
+
+
+@app.route('/', strict_slashes=False)
+def hello_HBNB():
+    """
+    print Hello HBNB! by returning it
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def HBNB():
+    """
+    print HBNB! by returning it
+    """
+    return 'HBNB'
+
+
+@app.route('/c/<word_text>', strict_slashes=False)
+def display_C(word_text):
+    """
+    this function displays C
+    """
+    return 'C ' + escape(word_text.replace('_', ' '))
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def display_python(text='is cool'):
+    """
+    this is a about python and not c
+    """
+    return 'Python ' + escape(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def display_n(n):
+    """display an integer"""
+    return str(n) + ' is a number'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
